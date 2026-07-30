d526b47e285198a62b47399e4fe3033d400fd323
Shubham Bhavsar <shubhamcbhavsar198@gmail.com>
2026-07-31 02:12:47 +0530

    feat: latest changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shubham-bhavsar/', // Set the base path for your app
+  base: '/shubham-bhavsar-hero/', // Set the base path for your app
 })
