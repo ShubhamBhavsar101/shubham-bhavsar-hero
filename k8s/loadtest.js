c947a8ed61b46d77ca8a0d0edaff6615072b5b1c
Shubham Bhavsar <shubhamcbhavsar198@gmail.com>
2026-08-04 08:21:44 +0530

    feat: added kubernetes

--- k8s/loadtest.js
+++ k8s/loadtest.js
@@ -1,15 +1,24 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 50,          // 50 concurrent virtual users
-  duration: '2m',   // Run for 2 minutes
+  vus: 1000,
+  duration: '150s',
 };
 
 export default function () {
-  const res = http.get('http://localhost:30080');
+  const params = {
+    headers: {
+      'Connection': 'keep-alive',
+    },
+  };
+  const res = http.get('http://portfolio.shubham.com/', params);
 
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
+
+  sleep(0.1);
+
+  // sleep(0.05); // 50ms pause per VU to avoid macOS TCP port exhaustion
 }
